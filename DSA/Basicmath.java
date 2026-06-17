2bf9cfe7b6e42e90ef80635db42d4ac488274ba7
vishwakbalaji-a <vishwakbalaji2304@gmail.com>
2026-06-17 21:35:36 +0530

    reverse a number

--- DSA/Basicmath.java
+++ DSA/Basicmath.java
@@ -1,4 +1,15 @@
 package CoreJava.DSA;
-
+import java.util.*;
 public class Basicmath {
+    public static void main(String [] args){
+        Scanner scan = new Scanner (System.in);
+        int n = scan.nextInt();
+        int rev = 0;
+        while(n>0){
+            int div = n%10;
+            rev = rev * 10 + div;
+            n/=10;
+        }
+        System.out.println(rev);
+    }
 }
