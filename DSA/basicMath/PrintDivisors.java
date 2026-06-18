753502fafb620570d9438d36dcbe9db742c5c9b4
vishwakbalaji-a <vishwakbalaji2304@gmail.com>
2026-06-18 17:55:29 +0530

    printing the divisors

--- DSA/basicMath/PrintDivisors.java
+++ DSA/basicMath/PrintDivisors.java
@@ -7,14 +7,18 @@ import static java.lang.Math.sqrt;
 public class PrintDivisors {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        ArrayList <Integer> list = new ArrayList<>();
         int num = scan.nextInt();
         for(int i = 1 ; i<=sqrt(num) ; i++){
             if(num%i == 0){
-                System.out.println(i);
-                if(num%i !=i){
-                    System.out.println(num/i);
+                list.add(i);
+                if(num/i!= i){
+                    list.add(num/i);
                 }
+
             }
         }
+        Collections.sort(list);
+        System.out.println(list);
     }
 }
