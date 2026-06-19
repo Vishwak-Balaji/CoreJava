7993bcb68e5c5d52062ed1ea0fd87c31c81cb0c1
vishwakbalaji-a <vishwakbalaji2304@gmail.com>
2026-06-20 00:15:42 +0530

    Basic patterns

---
+++ DSA/Patterns/Pattern6.java
@@ -0,0 +1,14 @@
+package CoreJava.DSA.Patterns;
+import java.util.*;
+public class Pattern6 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        for (int i=num ; i>=0 ; i--){
+            for(int j=1 ; j<=i ; j++){
+                System.out.print(j +" ");
+            }
+            System.out.println();
+        }
+    }
+}
